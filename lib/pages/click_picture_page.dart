80b38a8f6eb6b0562e5c4255a4e7cac05e7a8fff
Farhan <mdfarhan422@gmail.com>
2023-07-02 21:22:52 +0530

    added camera functionality in a circular preview

--- lib/pages/click_picture_page.dart
+++ lib/pages/click_picture_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:alemeno_food_game/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ClickPicturePage extends StatefulWidget {
   const ClickPicturePage({super.key});
@@ -9,8 +13,105 @@ class ClickPicturePage extends StatefulWidget {
 }
 
 class _ClickPicturePageState extends State<ClickPicturePage> {
+  late CameraController _cameraController;
+  late List<CameraDescription> _cameras;
+  // late String _imagePath;
+  File? _imagePath;
+  bool isInitialized = false;
+  bool isError = false;
+  String errorMessage = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializeCamera();
+  }
+
+  Future<void> initializeCamera() async {
+    _cameras = await availableCameras();
+    _cameraController = CameraController(
+      _cameras[0], // You can choose a specific camera here (front/rear)
+      ResolutionPreset.high,
+      imageFormatGroup: ImageFormatGroup.yuv420,
+    );
+    // await _cameraController.initialize();
+    // setState(() {
+    //   isInitialized = true;
+    // });
+    _cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        isInitialized = true;
+      });
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            setState(() {
+              isError = true;
+              errorMessage =
+                  '${e.code} - ${e.description}\nPlease allow permissions from App Settings';
+            });
+
+            print(errorMessage);
+
+            break;
+          default:
+            // Handle other errors here.
+            setState(() {
+              isError = true;
+              errorMessage = '${e.code} - ${e.description}';
+            });
+            print(errorMessage);
+            break;
+        }
+      }
+    });
+  }
+
+  Future<void> takePicture() async {
+    if (!_cameraController.value.isInitialized) {
+      return;
+    }
+
+    try {
+      final XFile imageFile = await _cameraController.takePicture();
+
+      final directory = await getApplicationDocumentsDirectory();
+      final fileName = 'my_image.jpg';
+      final savedImagePath = '${directory.path}/$fileName';
+      print("hellooo");
+
+      await File(imageFile.path).copy(savedImagePath);
+
+      setState(() {
+        _imagePath = File(imageFile.path);
+      });
+      print('Image File Location: ${imageFile.path}');
+      print('Image File : $imageFile');
+    } catch (e) {
+      print('Error capturing image: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Widget cameraPreview = CameraPreview(_cameraController);
+    if (!isInitialized) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
     return Scaffold(
       backgroundColor: Pallete.whiteColor,
       body: SingleChildScrollView(
@@ -80,6 +181,8 @@ class _ClickPicturePageState extends State<ClickPicturePage> {
                               Image.asset(
                                 'assets/images/cutleryImg.png',
                               ),
+                              // !cameraController.value.isInitialized ?
+
                               Positioned(
                                 left: 50,
                                 top: -25,
@@ -88,6 +191,50 @@ class _ClickPicturePageState extends State<ClickPicturePage> {
                                   width: 240,
                                 ),
                               ),
+
+                              if (!isInitialized) // Display a placeholder while the camera is initializing
+                                const Positioned(
+                                  top: -15,
+                                  left: 70,
+                                  child: CircleAvatar(
+                                    backgroundColor: Pallete.cameraPreviewColor,
+                                    radius: 100,
+                                  ),
+                                )
+                              else if (_imagePath ==
+                                  null) // Display the camera preview
+                                Positioned(
+                                  top: -15,
+                                  left: 70,
+                                  child: ClipOval(
+                                    child: SizedBox(
+                                      height: 200,
+                                      width: 200,
+                                      child: AspectRatio(
+                                        aspectRatio: 1,
+                                        child: CameraPreview(_cameraController),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              else // Display the captured image
+                                Positioned(
+                                  top: -15,
+                                  left: 70,
+                                  child: ClipOval(
+                                    child: SizedBox(
+                                      height: 200,
+                                      width: 200,
+                                      child: AspectRatio(
+                                        aspectRatio: 1,
+                                        child: Image.file(
+                                          _imagePath!,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
                             ],
                           ),
                         ),
@@ -106,7 +253,9 @@ class _ClickPicturePageState extends State<ClickPicturePage> {
                           height: 80,
                           child: FloatingActionButton(
                             backgroundColor: Pallete.greenColor,
-                            onPressed: () {},
+                            onPressed: () {
+                              takePicture();
+                            },
                             child: const Icon(
                               Icons.camera_alt_sharp,
                               size: 45,
--- lib/pallete.dart
+++ lib/pallete.dart
@@ -5,4 +5,5 @@ class Pallete {
   static const greenColor = Color(0xFF3E8B3A);
   static const greyColor = Color(0xFFF4F4F4);
   static const blackTextColor = Color(0xFFC3C3C);
+  static const cameraPreviewColor = Color(0xFF404040);
 }
